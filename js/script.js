c7dfe2224f20b20a6d5a511323e2787dc799316e
Your Name <xodjamovaminjon@gmail.com>
2022-11-18 21:32:52 +0500

    commit

--- js/script.js
+++ js/script.js
@@ -1,62 +1,56 @@
-let inpNeeds = document.querySelectorAll('.myBlue input')
-let allInps = document.querySelectorAll('form input')
-let btn = document.querySelector('button')
 let form = document.forms.reg
+let inps = form.querySelectorAll('input')
 
-let pattern = {
-    name: /^[a-z ,.'-]+$/i,// Y
-    surname: /^[a-z ,.'-]+$/i,//Y
-    password: /^((?=.*[a-z])|(?=.*[A-Z])|(?=.*\\d)|(?=.*(_|[-+_!@#$%^&*.,?]))).{6,12}/,//Y
-    phone: /^998[012345789][0-9]{8}$/g,// Y
-    email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-    aboutYou: /^[a-z ,.'-]+$/i,
-    age: /^(0?[1-9]|[1-9][0-9]|[1][1-9][1-9]|200)$/,
-    html: /^[a-z ,.'-]+$/i,// Y
-    css: /^[a-z ,.'-]+$/i,// Y
-    js: /^[a-z ,.'-]+$/i,// Y
-    favouriteCar: /^[a-z ,.'-]+$/i,// Y
+let regex = {
+    name: /^[a-z ,.'-]+$/i,
+    phone: /^998[012345789][0-9]{8}$/g
 }
 
 
 function validate(regex, field) {
     if (regex.test(field.value)) {
-        field.parentElement.classList.remove('invalid')
-        field.parentElement.classList.add('valid')
+        console.log(field);
+        field.style.border = "2px solid green"
     } else {
-        field.parentElement.classList.remove('valid')
-        field.parentElement.classList.add('invalid')
+        console.log(field);
+        field.style.border = "2px solid red"
     }
+
 }
 
-allInps.forEach(inp => {
-    inp.onkeyup = () => {
-
-        validate(pattern[inp.name], inp)
+inps.forEach(inp => {
+    // inp.onkeyup = () =>{
+    //   validate(regex[inp.name], inp)
+    // }
+    inp.onchange = () => {
+        console.log(inp.value);
     }
-})
-
+});
 
 form.onsubmit = (event) => {
-    event.preventDefault()
-    let countErrs = 0
+    event.preventDefault();
+    let errorss = 0
 
-    inpNeeds.forEach(inp => {
-        if (inp.value.length < 1 || inp.parentElement.classList.contains('invalid')) {
-            inp.parentElement.classList.add('invalid')
-            countErrs++
-        } else {
-            inp.parentElement.classList.remove('invalid')
+    inps.forEach(inp => {
+        inp.classList.remove('invalid')
+        if (inp.value.length < 1) {
+
+            inp.classList.add('invalid')
+            errorss++
         }
-    });
+    })
 
-    if (countErrs > 0) {
-        console.log('Error')
+
+        ;
+
+
+    if (errorss > 0) {
+        console.log('error');
     } else {
         submit(form)
     }
 
 }
-
 
 
 function submit(formElement) {
@@ -71,3 +65,111 @@ function submit(formElement) {
     console.log(user);
 }
 
+
+
+let slide = document.querySelectorAll('.offer__slide')
+let close = document.querySelector('.offer__slider-prev')
+let next = document.querySelector('.offer__slider-next')
+let text = document.querySelector('#current')
+let total = document.querySelector('#total')
+
+
+let slide_next = 0
+
+close.onclick = () => {
+    if (slide_next === 0) {
+    } else {
+        slide_next--
+        shou_slide(slide_next)
+    }
+
+}
+
+
+next.onclick = () => {
+    if (slide_next === 3) {
+    } else {
+        slide_next++
+        shou_slide(slide_next)
+    }
+}
+
+
+function shou_slide(nc) {
+
+    if (nc >= slide.length) {
+        slide_next = 0
+    }
+
+    if (nc == -1) {
+        slide_next = slide.length - 1
+    }
+    slide.forEach((slide) => {
+        slide.style.display = "none"
+    })
+    slide[slide_next].style.display = "block"
+    text.innerHTML = '0' + (slide_next + 1)
+}
+
+shou_slide()
+
+
+// timer
+
+
+const deadline = 'December 2022 31 23:59'
+
+
+function getTime(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+        days = Math.round(t / (1000 * 60 * 60 * 24)),
+        hours = Math.round((t / (1000 * 60 * 60)) % 24),
+        minutes = Math.round((t / 1000 / 60) % 60),
+        seconds = Math.round((t / 1000) % 60);
+
+    return {
+        t,
+        days,
+        hours,
+        minutes,
+        seconds
+    }
+}
+
+function setTime(selector, endtime) {
+    const timer = document.querySelector(selector),
+        days = timer.querySelector('#days'),
+        hours = timer.querySelector('#hours'),
+        minutes = timer.querySelector('#minutes'),
+        seconds = timer.querySelector('#seconds'),
+        updateInt = setInterval(updateTime, 1)
+
+
+    function updateTime() {
+        const t = getTime(endtime)
+
+        days.innerHTML = t.days
+        hours.innerHTML = t.hours
+        minutes.innerHTML = t.minutes
+        seconds.innerHTML = t.seconds
+
+        if (t.t <= 0) {
+            clearInterval(updateInt)
+        }
+    }
+
+}
+
+setTime('.timer', deadline)
+showSlides(idx)
+
+function showSlides(n) {
+    sliders.forEach(el => el.style.display = "none")
+
+    sliders[n].style.display = "block"
+}
+
+next.onclick = () => {
+    idx++
+    showSlides(idx)
+}
